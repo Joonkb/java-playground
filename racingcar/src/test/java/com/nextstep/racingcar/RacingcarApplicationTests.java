a1f4f1adf8d24a3bdfaf700c1cee85b1fcbb9ecd
Joonkb <byeongjunkim@naver.com>
2023-09-17 00:17:47 +0900

    racingcar-playground 환경세팅

---
+++ racingcar/src/test/java/com/nextstep/racingcar/RacingcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextstep.racingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
