a6e4ca2b0d5677973084ea9ad0b15f91572ff72d
Joonkb <byeongjunkim@naver.com>
2023-10-11 12:52:20 +0900

    [feat] 좌표파싱 TestCase 실패

---
+++ coordinate/src/main/java/coordinate/utils/ParsingUtils.java
@@ -0,0 +1,9 @@
+package coordinate.utils;
+
+import coordinate.domain.Point;
+
+public class ParsingUtils {
+    public static Point parse(String s) {
+        return null;
+    }
+}
---
+++ coordinate/src/test/java/coordinate/utils/ParsingUtilsTest.java
@@ -0,0 +1,19 @@
+package coordinate.utils;
+
+import coordinate.domain.Point;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ParsingUtilsTest {
+
+    @Test
+    @DisplayName("좌표 하나를 파싱하는 기능")
+    void 파싱_좌표하나() {
+        Point p = new Point(12, 23);
+        Point parsed = ParsingUtils.parse("(12,23)");
+        Assertions.assertThat(p).isEqualTo(parsed);
+    }
+
+
+}
