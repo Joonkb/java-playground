6b5bc9dd7359adfb363fb26bda71f0c58251351f
Joonkb <byeongjunkim@naver.com>
2023-10-22 23:19:44 +0900

    [feat] 에러 문구를 출력하고 다시 입력을 받도록 수정.

--- coordinate/src/main/java/coordinate/CoordinateMain.java
+++ coordinate/src/main/java/coordinate/CoordinateMain.java
@@ -11,11 +11,22 @@ import static coordinate.view.InputView.*;
 public class CoordinateMain {
 
     public static void main(String[] args) {
-        play();        
+        gameStart();
+    }
+
+    private static void gameStart() {
+        while (true) {
+            try {
+                play();
+                break;
+            } catch (Exception e) {
+                System.out.println("Error: [" + e.getMessage() + "] ");
+            }
+        }
     }
 
     private static void play() {
-        // 팩토리 메서드 패턴 적용 (패턴에서 Shape을 구현한 클래스 인스턴스 반환).
+        // 팩토리 메서드 패턴 적용 (팩토리에서 Shape을 구현한 클래스 인스턴스 반환).
         Shape shape = ShapeCreator.createShape(parse(getInput()));
         DrawView.drawCoordinate(shape);
         ResultView.printCalculatedArea(shape);
