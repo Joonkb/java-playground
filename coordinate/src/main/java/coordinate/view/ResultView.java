ab9d8b1a4f42b6a684c5a9f957f7eedf5b2a7a0f
Joonkb <byeongjunkim@naver.com>
2023-10-29 00:41:28 +0900

    [init] Branch init

--- coordinate/src/main/java/coordinate/view/ResultView.java
+++
@@ -1,9 +0,0 @@
-package coordinate.view;
-
-import coordinate.domain.Shape;
-
-public class ResultView {
-    public static void printCalculatedArea(Shape shape) {
-        System.out.print(shape);
-    }
-}
--- coordinate/src/main/java/fuel/domain/K5.java
+++
@@ -1,9 +0,0 @@
-package fuel.domain;
-
-
-public class K5 extends Car{
-
-    public K5(int dist) {
-        super(dist, CarModel.K5);
-    }
-}
