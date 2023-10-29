3e8ef30564e59e147598e6910e6b46b95287ee35
Joonkb <byeongjunkim@naver.com>
2023-10-29 15:17:19 +0900

    [feat] parsingTest 코드작성 및 기능구현

--- coordinate/src/main/java/coordinate/utils/ParsingUtils.java
+++ coordinate/src/main/java/coordinate/utils/ParsingUtils.java
@@ -4,10 +4,27 @@ import coordinate.domain.Point;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ParsingUtils {
+
+    private static final Pattern pattern = Pattern.compile("\\((\\d+),(\\d+)\\)"); // 좌표를 추출하기 위한 정규표현식.
+    private static final String DELIMITER = "-";
+
     public static List<Point> parse(String s) {
         List<Point> pointList = new ArrayList<>();
+        for (String xy : s.split(DELIMITER)) {
+            pointList.add(createPoint(xy));
+        }
         return pointList;
+    }
+
+    private static Point createPoint(String xy) {
+        Matcher matcher = pattern.matcher(xy);
+        if (matcher.matches()) {
+            return new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+        }
+        throw new IllegalArgumentException("올바른 좌표 형식이 아닙니다.");
     }
 }
--- coordinate/src/test/java/coordinate/utils/ParsingUtilsTest.java
+++ coordinate/src/test/java/coordinate/utils/ParsingUtilsTest.java
@@ -10,10 +10,19 @@ import java.util.List;
 class ParsingUtilsTest {
 
     @Test
-    @DisplayName("")
-    void 파싱_후_포인트_반환() {
+    @DisplayName("좌표 1개 추출")
+    void 파싱_후_포인트_리스트_반환_01() {
         Point p = new Point(2, 3);
         List<Point> parsedList = ParsingUtils.parse("(2,3)");
         Assertions.assertThat(parsedList).containsOnly(p);
+    }
+
+    @Test
+    @DisplayName("좌표 2개 추출")
+    void 파싱_후_포인트_리스트_반환_02() {
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(4, 3);
+        List<Point> parsedList = ParsingUtils.parse("(2,3)-(4,3)");
+        Assertions.assertThat(parsedList).containsOnly(p1, p2);
     }
 }
