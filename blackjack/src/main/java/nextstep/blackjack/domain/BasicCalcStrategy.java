383b086eb708038260aa186c56f4b44aeca279b5
Joonkb <byeongjunkim@naver.com>
2023-12-04 12:02:44 +0900

    [refactor] 수익계산 전략패턴으로 추출

--- blackjack/src/main/java/nextstep/blackjack/domain/BasicCalcStrategy.java
+++ blackjack/src/main/java/nextstep/blackjack/domain/BasicCalcStrategy.java
@@ -1,8 +1,31 @@
 package nextstep.blackjack.domain;
 
 public class BasicCalcStrategy implements CalcStrategy {
+
     @Override
-    public Integer calculatePlayersPoint(User dealer, Player player) {
-        return null;
+    public  Integer calculatePlayersPoint(User dealer, Player player) {
+
+        boolean dealerAddedYn       = dealer.hasTwoCardsYn();
+        boolean playerAddedYn       = player.hasTwoCardsYn();
+        boolean dealerBlackJackYn   = dealer.isBlackJackScore();
+        boolean playerBlackJackYn   = player.isBlackJackScore();
+
+        /**  딜러, 플레이어 모두 카드 2장에 블랙잭인경우 **/
+        if (dealerAddedYn && dealerBlackJackYn && playerAddedYn && playerBlackJackYn) {
+            return player.getBettingAmount();
+        }
+        /**  플레이어가 처음받은 2장이 블랙잭인 경우  **/
+        if (playerAddedYn && playerBlackJackYn) {
+            return player.getBettingAmount() * 15 / 10;
+        }
+        /** 딜러의 점수가 21점을 초과한 경우 **/
+        if (dealer.exceedBlackJackScoreYn()) {
+            return player.getBettingAmount();
+        }
+        /** 플레이어의 점수가 21점을 초과한 경우 **/
+        if (player.exceedBlackJackScoreYn()) {
+            return player.getBettingAmount() * -1;
+        }
+        return player.getBettingAmount() * -1;
     }
 }
--- blackjack/src/main/java/nextstep/blackjack/utils/GameManager.java
+++ blackjack/src/main/java/nextstep/blackjack/utils/GameManager.java
@@ -1,8 +1,6 @@
 package nextstep.blackjack.utils;
 
-import nextstep.blackjack.domain.Deck;
-import nextstep.blackjack.domain.Player;
-import nextstep.blackjack.domain.User;
+import nextstep.blackjack.domain.*;
 
 import java.util.List;
 import java.util.Scanner;
@@ -14,6 +12,8 @@ import java.util.stream.Collectors;
 public class GameManager {
 
     private static Scanner sc = new Scanner(System.in);
+
+    private static CalcStrategy strategy = new BasicCalcStrategy();
 
     public static void divideTwoCards(List<Player> players, User dealer, Deck deck) {
 
@@ -80,41 +80,12 @@ public class GameManager {
         System.out.println("## 최종 수익");
         int cnt = 0;
         List<Integer> profits = players.stream()
-                .map(player -> calculatePlayersProfit(dealer, player))
+                .map(player -> strategy.calculatePlayersPoint(dealer, player))
                 .collect(Collectors.toList());
         Integer dealerProfit = profits.stream().reduce(Integer::sum).get() * -1;
         System.out.println("딜러: " + dealerProfit);
         for (Player player : players) {
             System.out.println(player.getName() + ": " + profits.get(cnt++));
         }
-    }
-
-    /**
-     *   우선순위를 고려한 수익금 반환
-     */
-    public static Integer calculatePlayersProfit(User dealer, Player player) {
-
-        boolean dealerAddedYn       = dealer.hasTwoCardsYn();
-        boolean playerAddedYn       = player.hasTwoCardsYn();
-        boolean dealerBlackJackYn   = dealer.isBlackJackScore();
-        boolean playerBlackJackYn   = player.isBlackJackScore();
-
-        /**  딜러, 플레이어 모두 카드 2장에 블랙잭인경우 **/
-        if (dealerAddedYn && dealerBlackJackYn && playerAddedYn && playerBlackJackYn) {
-            return player.getBettingAmount();
-        }
-        /**  플레이어가 처음받은 2장이 블랙잭인 경우  **/
-        if (playerAddedYn && playerBlackJackYn) {
-            return player.getBettingAmount() * 15 / 10;
-        }
-        /** 딜러의 점수가 21점을 초과한 경우 **/
-        if (dealer.exceedBlackJackScoreYn()) {
-            return player.getBettingAmount();
-        }
-        /** 플레이어의 점수가 21점을 초과한 경우 **/
-        if (player.exceedBlackJackScoreYn()) {
-            return player.getBettingAmount() * -1;
-        }
-        return player.getBettingAmount() * -1;
     }
 }
